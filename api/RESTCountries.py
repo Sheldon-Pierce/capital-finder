543482e5b3ef877bb11113f19bb99866813f01f0
Sheldon Pierce <Pierce55@icloud.com>
2023-04-10 18:53:09 -0700

    testing pt.3 for time

--- api/RESTCountries.py
+++ api/RESTCountries.py
@@ -1,13 +1,12 @@
 from http.server import BaseHTTPRequestHandler
+from datetime import datetime
 
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        message = "string_test"
-        print(message)
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        # message = "Hello World, Roger's Serverless Method"
-        self.wfile.write(message.encode())
+        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
         return
