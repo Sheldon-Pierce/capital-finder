35172d4e89837694273402c6ac038cb94ce830f7
Sheldon Pierce <Pierce55@icloud.com>
2023-04-10 18:48:26 -0700

    testing

---
+++ api/RESTCountries.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = "string_test"
+        print(message)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        message = "Hello World, Roger's Serverless Method"
+        self.wfile.write(message.encode())
+        return
